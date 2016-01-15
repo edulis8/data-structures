5cb03b680dec4c3baa134c47077185ba12cb369e
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-01-15 14:15:08 -0800

    Complete linked list, Eric navigater, pure function recursion.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,36 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    var result = list.head;
+    delete list.head;
+    list.head = result.next;
+    return result.value;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, node){
+    // check if target === node.value
+    // if not, recursively check the next node
+    node = node || list.head;
+    if (node.value === target) {
+      return true;
+    } else {
+      if (node.next){
+        return list.contains(target, node.next);
+      }
+    }
+    return false;
   };
 
   return list;
