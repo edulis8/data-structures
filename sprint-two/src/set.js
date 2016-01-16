7bae5a0579473bfe8c68b9b7d55eb83db2a255b6
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-01-15 20:41:27 -0800

    Complete set passing all tests.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
+// prototype pattern
+
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+
+  set._storage = {};
+
   return set;
 };
 
 var setPrototype = {};
-
+// An .add() method, takes any string and adds it to the set
 setPrototype.add = function(item){
+  this._storage[item] = null;
 };
-
+// A .contains() method, takes any string and returns a
+// boolean reflecting whether it can be found in the set
 setPrototype.contains = function(item){
+  return item in this._storage // Who you gunna call?
 };
-
+// A .remove() method, takes any string and removes it from the set, if present
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
