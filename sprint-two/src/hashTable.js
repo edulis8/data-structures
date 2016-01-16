eb06ead6cf5a121768b8cb3a614edc8bd4a2dea6
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-01-16 15:59:10 -0800

    ...

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,78 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // retrieve a bucket
+  var bucket = this._storage.get(i);
+  // if !exist, create it
+  if (! bucket ){
+    bucket = [];
+    this._storage.set(i, bucket);
+  }
+
+  var found = false;
+
+  // iterate over bucket
+  for (var j = 0; j < bucket.length; i++) {
+    var tuple = bucket[j];
+    // key exists?
+    if(tuple[0] === k){
+      // update it
+      tuple[1] = v;
+      found = true;
+      break;
+    }
+  }
+
+  // if no key was foun
+  if (!found){
+    // insert a new tuple
+    bucket.push([k,v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  // retrieve a bucket
+  var bucket = this._storage.get(i);
+  // if !exist, create it
+  if (! bucket ){
+    return null;  
+  }
+
+  // iterate over bucket
+  for (var j = 0; j < bucket.length; j++) {
+    var tuple = bucket[j];
+    if(tuple[0] === k){
+      return tuple[1];
+    }
+  }
+
+  return null;
+
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // retrieve a bucket
+  var bucket = this._storage.get(i);
+  // if !exist, create it
+  if (! bucket ){
+    return null;  
+  }
+
+  // iterate over bucket
+  for (var j = 0; j < bucket.length; j++) {
+    var tuple = bucket[j];  
+    if(tuple[0] === k){
+       bucket.splice(j, 1);
+       return tuple[1]; 
+    }
+  }
+
+  return null;
 
 };
 
